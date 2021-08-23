50a1258aa26170ff200a539e75e7039c08b79cb8
Zach Nicoll <zach.nicoll55@gmail.com>
2021-08-23 22:30:24 +1000

    feat: add helmet for security, and morgan for logging

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 import routes from "./routes";
 import config from "./utils/config";
 
 const app = express();
+const logger = morgan("short");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(logger);
 
 app.use(routes);
 
